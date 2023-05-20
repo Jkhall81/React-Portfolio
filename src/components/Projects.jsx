8aad0ccb3011924f0780783bf1db97410a77fae5
Jkhall81 <jason.kei.hall@gmail.com>
2023-05-20 12:51:08 -0700

    fixed issue in the projects page where the h3 and paragraph text were not displaying properly.  Project page is done for now.  Links work.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -16,33 +16,34 @@ const Projects = () => {
         style={{ backgroundImage: `url(${backGImg})`, backgroundSize: "cover" }}
       ></div>
       <div className="w-full h-screen absolute top-0 left-0 bg-black/30 bg-gradient-to-r from-black"></div>
-      <h1 className="text-4xl font-bold text-center text-[#001b5e]">
-        Projects
-      </h1>
-      <p className="text-center py-8">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum odit
-        sapiente impedit velit eaque cumque eius facilis aut repellendus nobis,
-        doloribus amet nihil inventore quis deleniti consequatur, dolore neque
-        quia.
-      </p>
-      <div className="grid sm:grid-cols-2 gap-12">
-        <ProjectItem
-          img={cryptoImg}
-          title="Crypto Page"
-          link="https://cryptobase-f3acf.firebaseapp.com/"
-        />
+      <div className="relative">
+        <h1 className="text-4xl font-bold text-center text-white">Projects</h1>
+        <p className="text-center py-8 text-white">
+          These projects are showcases of my front-end work, demonstrating the
+          power of React and the versatility of Tailwind CSS. Leveraging the
+          capabilities of APIs, these applications seamlessly integrate
+          up-to-date data, providing users with dynamic and real-time
+          information.
+        </p>
+        <div className="grid sm:grid-cols-2 gap-12">
+          <ProjectItem
+            img={cryptoImg}
+            title="Crypto Page"
+            link="https://cryptobase-f3acf.firebaseapp.com/"
+          />
 
-        <ProjectItem
-          img={netflix}
-          title="Netflix Page"
-          link="https://melodious-lollipop-6e6ded.netlify.app/"
-        />
+          <ProjectItem
+            img={netflix}
+            title="Netflix Page"
+            link="https://melodious-lollipop-6e6ded.netlify.app/"
+          />
 
-        <ProjectItem
-          img={dataImg}
-          title="Data Security Page"
-          link="https://spiffy-squirrel-eb6e18.netlify.app/"
-        />
+          <ProjectItem
+            img={dataImg}
+            title="Data Security Page"
+            link="https://spiffy-squirrel-eb6e18.netlify.app/"
+          />
+        </div>
       </div>
     </div>
   );
