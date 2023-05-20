35e47d0e58c87ad53b9261f61f5b331c53aef394
Jkhall81 <jason.kei.hall@gmail.com>
2023-05-20 07:27:53 -0700

    started project component.

---
+++ src/components/Projects.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ProjectItem from "./ProjectItem";
+// import images to be used
+
+const Projects = () => {
+  return <div>Projects</div>;
+};
+
+export default Projects;
