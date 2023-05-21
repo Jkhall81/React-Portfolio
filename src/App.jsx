1f0b6c94959f639d6bd87742d9f944f71fb9ede7
Jkhall81 <jason.kei.hall@gmail.com>
2023-05-20 19:11:19 -0700

    almost finished with the form used to send me a message.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Sidenav from "./components/Sidenav";
 import Main from "./components/Main";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Main />
       <Skills />
       <Projects />
+      <Contact />
     </div>
   );
 }
