31f5d4c29580b0ea64a845aabd79a242f96ace51
Jkhall81 <jason.kei.hall@gmail.com>
2023-05-21 16:52:57 -0700

    fixed skill section so that icons are no longer running down into the projects page when the screen width goes below the small breakpoint.  The skill section is now completely responsive.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,28 +7,26 @@ const Skills = () => {
   return (
     <div
       id="skills"
-      className="w-full m-auto md:pl-20 md:pr-20 p-4 py-16 relative h-screen"
+      className="w-full m-auto md:pl-20 md:pr-20 p-4 py-16 relative min-h-screen flex items-center justify-center"
     >
       <div
-        className="absolute top-0 left-0 h-screen w-full bg-cover"
+        className="absolute top-0 left-0 h-full w-full bg-cover"
         style={{ backgroundImage: `url(${skillz})`, backgroundSize: "cover" }}
       ></div>
-      <div className="w-full h-screen absolute top-0 left-0 bg-black/30 bg-gradient-to-r from-black"></div>
-      <div className="relative z-10 text-white flex flex-wrap justify-center h-full">
+      <div className="w-full h-full absolute top-0 left-0 bg-black/30 bg-gradient-to-r from-black"></div>
+      <div className="relative z-10 text-white flex flex-wrap justify-center">
         <h1 className="text-4xl font-bold text-center w-full mb-8">Skills</h1>
-        {data.map((item, idx) => (
-          <div
-            key={idx}
-            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4 flex justify-center"
-          >
-            <SkillItem
-              key={idx}
-              title={item.title}
-              experience={item.experience}
-              icon={item.icon}
-            />
-          </div>
-        ))}
+        <div className="w-full md:w-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-14">
+          {data.map((item, idx) => (
+            <div key={idx} className="flex justify-center">
+              <SkillItem
+                title={item.title}
+                experience={item.experience}
+                icon={item.icon}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
