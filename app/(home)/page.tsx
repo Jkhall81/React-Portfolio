810013d9f979cfe0809bdb7e398011b85785dc8b
Jkhall81 <jason.kei.hall@gmail.com>
2024-03-09 20:04:57 -0700

    added aria-labels to icon links with no text, accessibility increased.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -20,24 +20,28 @@ const links = [
     href: "https://www.facebook.com/profile.php?id=100081060077918",
     target: "_blank",
     color: "#316ff6",
+    "aria-label": "Facebook icon link",
   },
   {
     icon: FaLinkedinIn,
     href: "https://www.linkedin.com/in/jason-hall-650964242",
     target: "_blank",
     color: "#0a66c2",
+    "aria-label": "Linkedin icon link",
   },
   {
     icon: FaGithub,
     href: "https://github.com/Jkhall81",
     target: "_blank",
     color: "white",
+    "aria-label": "Github icon link",
   },
   {
     icon: FaWhatsapp,
     href: "https://api.whatsapp.com/send?phone=6232062944",
     target: "_blank",
     color: "#25d366",
+    "aria-label": "Whatsapp Icon link",
   },
 ];
 
@@ -97,7 +101,11 @@ export default function Home() {
                   viewport={{ once: false, amount: 0.4 }}
                   key={index}
                 >
-                  <Link href={item.href} target={item.target}>
+                  <Link
+                    href={item.href}
+                    target={item.target}
+                    aria-label={item["aria-label"]}
+                  >
                     <LinkIcon color={item.color} size={35} />
                   </Link>
                 </motion.div>
