0da2aa1b503113b8f4ff217b06728bf045da242f
Jkhall81 <jason.kei.hall@gmail.com>
2024-03-11 05:12:59 -0700

    metadata title update.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    default: "Jason Hall - Software Engineer",
+    default: "Jason Hall - Software Engineer - Fullstack Engineer",
     template: "Jason Hall - %s",
   },
   description:
